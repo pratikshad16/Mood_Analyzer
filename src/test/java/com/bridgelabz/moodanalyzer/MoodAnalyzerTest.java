31c1a240e0e46edec4f05cede8544a898ee077e2
pratikshad16 <dhagepratiksha16@gmail.com>
2020-08-30 20:51:47 +0530

    Added: TC-1.2 any mood message returns happy

--- src/test/java/com/bridgelabz/moodanalyzer/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/moodanalyzer/MoodAnalyzerTest.java
@@ -5,9 +5,16 @@ import org.junit.Test;
 
 public class MoodAnalyzerTest {
     @Test
-    public void name() {
+    public void givenMessage_WhenSadMood_ShouldReturnSad() {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
         String mood = moodAnalyzer.analyzeMood("I am in Sad Mood");
         Assert.assertEquals("Sad",mood);
+    }
+
+    @Test
+    public void givenMessage_whenAnyMood_ShouldReturnHappy() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        String mood = moodAnalyzer.analyzeMood("I am in Any Mood");
+        Assert.assertEquals("Happy",mood);
     }
 }
